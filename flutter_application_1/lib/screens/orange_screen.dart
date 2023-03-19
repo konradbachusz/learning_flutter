d61137e929b50eca89bdd163902df173b122e9ff
konradbachusz <konradbachusz@gmail.com>
2023-03-19 08:11:47 +0000

    Added  wild cats images

--- flutter_application_1/lib/screens/orange_screen.dart
+++ flutter_application_1/lib/screens/orange_screen.dart
@@ -5,27 +5,73 @@ void main() => runApp(OrangeContainer());
 class OrangeContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Container',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Container'),
-          ),
-          body: Column(
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                margin: EdgeInsets.all(50),
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  shape: BoxShape.rectangle,
-                  image: DecorationImage(
-                      image: NetworkImage('http://bit.ly/flutter_tiger')),
-                ),
-              )
-            ],
-          )),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Container'),
+        ),
+        body: Row(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://openclipart.org/image/300px/svg_to_png/176055/044482e2.png')),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.brown,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://openclipart.org/image/800px/206013')),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://openclipart.org/image/800px/179198')),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://openclipart.org/image/800px/130843')),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://openclipart.org/image/800px/21054')),
+              ),
+            )
+          ],
+        ));
   }
 }
